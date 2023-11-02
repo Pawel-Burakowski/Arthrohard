34ced2c5c38a01c20fff4d8f7d8b0e145729d14a
Pawel-Burakowski <110766570+Pawel-Burakowski@users.noreply.github.com>
2023-11-02 07:25:00 +0100

    Project init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+	<HelloWorld />
+</template>
+
+<script>
+import HelloWorld from "../src/components/HelloWorld.vue"
+
+export default {
+	name: "App",
+
+	components: {
+		HelloWorld,
+	},
+
+	data: () => ({
+		//
+	}),
+}
+</script>
+
+<style></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,5 @@
+<template>
+	<h1 class="w-96 bg-white shadow rounded">Hello world!!!</h1>
+</template>
+
+<script setup></script>
