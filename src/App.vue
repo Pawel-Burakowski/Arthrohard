a7f4b977ea777a34d810fd3e65381e38588ccd1e
Pawel-Burakowski <110766570+Pawel-Burakowski@users.noreply.github.com>
2023-11-04 10:24:34 +0100

    Added Paralax Component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
 	<NavbarComponent />
 	<MainContent />
+	<ParalaxComponent />
 	
 </template>
 
 <script>
 import NavbarComponent from "../src/components/NavbarComponent.vue"
 import MainContent from "../src/components/MainContent.vue"
+import ParalaxComponent from "../src/components/ParalaxComponent.vue"
 
 export default {
 	name: "App",
 
 	components: {
 		NavbarComponent,
-		MainContent
+		MainContent,
+		ParalaxComponent
 	},
 
 	data: () => ({
---
+++ src/components/ParalaxComponent.vue
@@ -0,0 +1,22 @@
+<template>
+	<section class="overflow-hidden parallax">
+		<div class="mb-[20px] mt-[800px] items-center"></div>
+	</section>
+</template>
+
+<script>
+export default {
+	name: "ParalaxComponent",
+}
+</script>
+
+<style scoped>
+.parallax {
+	background-image: url("../images/piesek 1.png"),
+		url("../images/dog-vector.webp"), url("../images/Vector_11.png");
+	background-attachment: fixed, fixed, fixed;
+	background-repeat: no-repeat, no-repeat, no-repeat;
+	background-position: center, calc(50% - 200px) calc(60% + 80px),
+		calc(50% + 300px) calc(50% + 85px);
+}
+</style>
